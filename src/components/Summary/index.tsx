e51fbe431400921130a5d584f75f00ff92d9a67d
Paula Lopes <pls21@msn.com>
2021-03-28 13:21:26 -0300

    Context API: Transaction

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,13 +1,18 @@
+import { useContext } from 'react'
 import incomeImg from '../../assets/income.svg'
 import outcomeImg from '../../assets/outcome.svg'
 import totalImg from '../../assets/total.svg'
+import { TransactionContext } from '../../TransactionContext'
 
 import { Container } from './styles'
 
 export function Summary() {
   
+  const {transactions} = useContext(TransactionContext);
+
   return(
     <Container>
+      
       <div>
         <header>
           <p>Entradas</p>
